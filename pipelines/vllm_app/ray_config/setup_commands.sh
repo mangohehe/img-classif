b8b358dfe32657c48baa4294a2c99ee3fbd42990
mangohehe <2023isanewjourney@gmail.com>
2025-03-11 14:02:13 -0700

    rm bonda sh checking

--- pipelines/vllm_app/ray_config/setup_commands.sh
+++ pipelines/vllm_app/ray_config/setup_commands.sh
@@ -3,17 +3,6 @@
 # Log the start of the script
 echo "Starting setup_commands.sh" >> /tmp/init.log
 
-CONDA_DIR="/home/ray/miniforge3"
-
-# Source Conda initialization script
-CONDA_SH="$CONDA_DIR/etc/profile.d/conda.sh"
-if [ -f "$CONDA_SH" ]; then
-  source "$CONDA_SH" || { echo "Failed to initialize Conda" >> /tmp/init.log; exit 1; }
-else
-  echo "Conda initialization script not found at $CONDA_SH. Please ensure Conda is properly installed." >> /tmp/init.log
-  exit 1
-fi
-
 # Create and activate the Conda environment
 echo "Setting up Ray environment..." >> /tmp/init.log
 bash /home/ray/img-classif/pipelines/vllm_app/ray_config/setup_ray_env.sh >> /tmp/init.log 2>&1 || { echo "Failed to setup Ray environment" >> /tmp/init.log; exit 1; }
